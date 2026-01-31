e4ba377f4b5120559dfa5ef7aab6edbc73c679a9
NagaprajwalB <nagaprajwalbhyr@gmail.com>
2026-01-31 20:07:37 +0530

    initial commit

---
+++ Two_sum.py
@@ -0,0 +1,16 @@
+from typing import List 
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        num_dict = {}
+        for i, num in enumerate(nums):
+            complement = target - num
+            if complement in num_dict:
+                return [num_dict[complement], i]
+            num_dict[num] = i
+        return []
+
+if __name__ == "__main__":
+    s = Solution()
+    result = s.twoSum([2, 7, 11, 15], 9)
+    print(result)

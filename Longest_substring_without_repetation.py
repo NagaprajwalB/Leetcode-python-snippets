6916d3a8b08788c3a23c3cb4bb825b47bba5c7c1
NagaprajwalB <nagaprajwalbhyr@gmail.com>
2026-02-02 03:19:35 +0530

    Longest_substring_without_repetation code

---
+++ Longest_substring_without_repetation.py
@@ -0,0 +1,23 @@
+# Logic Part 
+
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        char_dict = {}
+        max_len = 0
+        start = 0
+
+        for end in range(len(s)):
+            if s[end] in char_dict:
+                start = max(start, char_dict[s[end]] + 1)
+            char_dict[s[end]] = end
+            max_len = max(max_len, end - start + 1)
+
+        return max_len
+    
+# Test Part
+
+if __name__ == "__main__":
+    sol = Solution()
+    print(sol.lengthOfLongestSubstring("abcabcbb"))
+    print(sol.lengthOfLongestSubstring("bbbbb"))
+    print(sol.lengthOfLongestSubstring("pwwkew"))
